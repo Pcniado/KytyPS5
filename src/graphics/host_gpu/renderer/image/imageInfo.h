bff1b7b297d277d31a6d0a820a42b084b2f9f557
Mac <me@itsmemac.com>
2026-09-23 01:14:58 +0530

    video out: add 10-bit and FP16 output format policies
    Adds video-out pixel format encodings titles register beside the ones already
    listed:
    
    - 0x8100000622000000, 0x8100070422000000: A2B10G10R10
    - 0x8100000600000000, 0x8100070400000000: A2R10G10B10
    - 0xc001070722000000, 0xc001070700000000: R16G16B16A16 float (the second with
      swapped channels, as for 0xc001000600000000)

--- src/graphics/host_gpu/renderer/image/imageInfo.h
+++ src/graphics/host_gpu/renderer/image/imageInfo.h
@@ -329,7 +329,19 @@ struct VideoOutFormatPolicy {
 	VideoOutPixelFormatInfo info;
 };
 
-inline constexpr std::array<VideoOutFormatPolicy, 6> VIDEO_OUT_FORMAT_POLICIES {{
+inline constexpr std::array<VideoOutFormatPolicy, 12> VIDEO_OUT_FORMAT_POLICIES {{
+    {0x8100000622000000ull,
+     {vk::Format::eA2B10G10R10UnormPack32, Prospero::BufferFormat::k10_10_10_2UNorm, 4, false}},
+    {0x8100000600000000ull,
+     {vk::Format::eA2R10G10B10UnormPack32, Prospero::BufferFormat::k10_10_10_2UNorm, 4, false}},
+    {0x8100070422000000ull,
+     {vk::Format::eA2B10G10R10UnormPack32, Prospero::BufferFormat::k10_10_10_2UNorm, 4, false}},
+    {0x8100070400000000ull,
+     {vk::Format::eA2R10G10B10UnormPack32, Prospero::BufferFormat::k10_10_10_2UNorm, 4, false}},
+    {0xc001070722000000ull,
+     {vk::Format::eR16G16B16A16Sfloat, Prospero::BufferFormat::k16_16_16_16Float, 8, false}},
+    {0xc001070700000000ull,
+     {vk::Format::eR16G16B16A16Sfloat, Prospero::BufferFormat::k16_16_16_16Float, 8, true}},
     {0x8000000022000000ull,
      {vk::Format::eR8G8B8A8Srgb, Prospero::BufferFormat::k8_8_8_8Srgb, 4, false}},
     {0x8000000000000000ull,
